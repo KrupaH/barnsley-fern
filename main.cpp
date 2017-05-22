8f89dfd1438b874165eb8323e0aadc2e935d84ed
KrupaHebbar <krupa.hebbar@gmail.com>
2017-05-22 09:59:58 +0530

    Added menu for different ferns

--- main.cpp
+++ main.cpp
@@ -12,9 +12,60 @@
 
 using namespace std;
 
-const float abcd[4][2][2] = {{{0,0},{0,0.16}},{{0.85,0.04},{-0.04,0.85}},{{0.2,-0.26},{0.23,0.22}},{{-0.15,0.28},{0.26,0.24}}};
-const float ef[4][2][1] = {{{0},{0}},{{0},{1.6}},{{0},{1.6}},{{0},{0.44}}};
-const float p[4] = {1,85,7,7};
+//1
+float barnsley_abcd[4][2][2] = {{{0,0},{0,0.16}},{{0.85,0.04},{-0.04,0.85}},{{0.2,-0.26},{0.23,0.22}},{{-0.15,0.28},{0.26,0.24}}};
+float barnsley_ef[4][2][1] = {{{0},{0.16}},{{0},{1.6}},{{0},{1.6}},{{0},{0.44}}};
+float barnsley_p[4] = {1,85,7,7};
+
+//2
+float barnsley_mod_abcd[4][2][2] = {{{0,0},{0,0.2}},{{0.845,0.035},{-0.035,0.82}},{{0.20,-0.31},{0.255,0.245}},{{-0.15,0.24},{0.25,0.20}}};
+float barnsley_mod_ef[4][2][1] = {{{0},{-0.12}},{{0},{1.6}},{{0},{0.29}},{{0},{0.68}}};
+float barnsley_mod_p[4] = {1,85,7,7};
+
+//3
+float cyclo_abcd[4][2][2] = {{{0,0},{0,0.25}},{{0.95,0.005},{-0.005,0.93}},{{0.035,-0.20},{0.16,0.04}},{{-0.04,0.2},{0.16,0.04}}};
+float cyclo_ef[4][2][1] = {{{0},{-0.4}},{{-0.002},{0.5}},{{-0.09},{0.02}},{{0.083},{0.12}}};
+float cyclo_p[4] = {2,84,7,7};
+
+//4
+float culcita_abcd[4][2][2] = {{{0,0},{0,0.16}},{{0.85,0.02},{-0.02,0.83}},{{0.09,-0.28},{0.30,0.11}},{{-0.09,0.28},{0.30,0.09}}};
+float culcita_ef[4][2][1] = {{{0},{-0.14}},{{0},{1}},{{0},{0.6}},{{0},{0.7}}};
+float culcita_p[4] = {2,84,7,7};
+
+float abcd[4][2][2];
+float ef[4][2][1];
+float p[4];
+
+bool isFirstDisplay = true;
+
+void assignFern(float abcd[4][2][2], float ef[4][2][1], float p[4]){
+    for(int i=0 ; i<4 ; i++){
+        for(int j=0 ; j<2 ; j++){
+            for(int k=0 ; k<2 ; k++){
+                ::abcd[i][j][k] = abcd[i][j][k];
+            }
+            ::ef[i][j][0] = ef[i][j][0];
+        }
+        ::p[i] = p[i];
+    }
+}
+
+void changeFern(const int changeLeaf){
+    switch(changeLeaf){
+        case 1: assignFern(barnsley_abcd, barnsley_ef, barnsley_p);
+                glutPostRedisplay();
+                break;
+        case 2: assignFern(barnsley_mod_abcd, barnsley_mod_ef, barnsley_mod_p);
+                glutPostRedisplay();
+                break;
+        case 3: assignFern(cyclo_abcd, cyclo_ef, cyclo_p);
+                glutPostRedisplay();
+                break;
+        case 4: assignFern(culcita_abcd, culcita_ef, culcita_p);
+                glutPostRedisplay();
+                break;
+    }
+}
 
 /* Helper method to perform matrix multiplication and addition
  * abcd[i]*{x,y}+ef[i]
@@ -52,11 +103,18 @@ void myinit()
       glMatrixMode(GL_MODELVIEW);
 }
 
+
 void display( void )
 {
     int j, k;
     glClearColor(1.0, 1.0, 1.0, 1.0); /* white background */
     glClear(GL_COLOR_BUFFER_BIT);  /*clear the window */
+
+    //initialization of abcd, ef, and p if first display
+    if(isFirstDisplay){
+
+        isFirstDisplay = false;
+    }
 
 
 /* compute and plots 5000 new points */
@@ -91,6 +149,13 @@ int main(int argc, char** argv)
     glutInitWindowPosition(25,25); /* place window top left on display */
     glutCreateWindow("Barnsley Fern"); /* window title */
     glutDisplayFunc(display); /* display callback invoked when window opened */
+
+    glutCreateMenu(changeFern);
+    glutAddMenuEntry("Barnsley",1);
+    glutAddMenuEntry("Barnsley_Mod",2);
+    glutAddMenuEntry("Cyclo",3);
+    glutAddMenuEntry("Culcita",4);
+    glutAttachMenu(GLUT_RIGHT_BUTTON);
 
     myinit(); /* set attributes */
 
